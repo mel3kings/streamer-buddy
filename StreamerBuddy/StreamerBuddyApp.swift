9894d586a1a24e62c9ca0f3f15a4b985da95e2df
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2023-11-06 14:21:16 +0700

    Initial Commit

---
+++ StreamerBuddy/StreamerBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamerBuddyApp.swift
+//  StreamerBuddy
+//
+//  Created by Melchor Tatlonghari on 6/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct StreamerBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
