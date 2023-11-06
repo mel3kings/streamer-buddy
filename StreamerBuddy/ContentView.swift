e12a314565f7c55fcfc90a1219fd5074babb3f70
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2023-11-06 15:00:52 +0700

    Initial Commit

--- StreamerBuddy/ContentView.swift
+++ StreamerBuddy/ContentView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username: String=""
+    @State private var showAlert = false
+    @State private var joke: String = ""
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello")
+            TextField(
+                    "Enter your Query",
+                    text: $username
+            ).onSubmit {
+                print("hello world")
+            }
+            Text(joke)
+            Button {
+                        Task {
+                            let (data, _) = try await URLSession.shared.data(from: URL(string:"https://api.chucknorris.io/jokes/random")!)
+                            let decodedResponse = try? JSONDecoder().decode(Joke.self, from: data)
+                            joke = decodedResponse?.value ?? ""
+                        }
+                    } label: {
+                        Text("Fetch Chuck Norris Joke")
+                    }
         }
         .padding()
     }
@@ -24,3 +43,9 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Joke: Codable {
+    let value: String
+}
+
+
